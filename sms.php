76b5477daee60ffbdc80a20d074772c806b5ba8c
Janagiramanm <janagiraman@netiapps.com>
2021-03-27 10:56:36 +0530

    debug

--- sms.php
+++ sms.php
@@ -3,7 +3,7 @@ if (!isset($_SESSION)) { session_start(); }
 include('connect.php');
 
 echo '<pre>';
-print_r($_POST);
+print_r($_POST); 
 
 if(isset($_POST['submit']) && !empty($_POST['submit'])){
     if(isset($_POST['g-recaptcha-response']) && !empty($_POST['g-recaptcha-response'])){
